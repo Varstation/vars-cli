5c6aa1b4b9ce90d1db326e142b6d7b47a3c8ee02
Michel Chieregato <michel.chieregato@gmail.com>
2022-08-01 11:08:57 -0300

    Remove all project

--- utils/constants.js
+++
@@ -1,9 +0,0 @@
-export const API_URL = '';
-export const HOMOLOG_URL = '';
-export const LOCAL_URL = 'http://localhost:8000/api/';
-
-export const TOKEN_PATH = './data/.token.json';
-export const AWS_CREDENTIALS_PATH = './data/.temp_credentials.json';
-
-export const AWS_BUCKET_NAME = 'vars-static-homolog';
-export const UPLOAD_FOLDER_NAME = 'vars-cli-uploads';
