5edbfbb7c412ef430fd2b11cd51370a7b14e06d7
Luan Vitor Coelho de Araujo <luan_vittor@hotmail.com>
2022-11-18 14:16:56 -0300

    Update functions.js

--- utils/functions.js
+++ utils/functions.js
@@ -1,5 +1,7 @@
 import fs from 'fs';
+import axios from 'axios';
 import { DATA_FOLDER_PATH } from './constants.js';
+import { AUTH_PATH, API_URL } from './constants.js';
 
 export const getJson = (filename) => {
     const path = `${DATA_FOLDER_PATH}/${filename}`;
@@ -21,3 +23,27 @@ const createDataDirectoryIfDoesntExists = () => {
         fs.mkdirSync(DATA_FOLDER_PATH);
     }
 };
+
+export const start_process = async (filePath) => {
+    const token = getJson(AUTH_PATH)?.token;
+    const csvFile = fs.createReadStream(filePath)
+    try {
+        await axios.post(
+            `${API_URL}sample/teste/`,
+            {
+                headers: {
+                    'Authorization': `Token ${token}`,
+                },
+                // csvFile,
+            }).then(
+            (response) => {
+                console.log(response.data)
+                return response.data
+            })
+    } catch (error) {
+        const err = error
+        if (err.response) {
+           console.log(err.response.status, err.response.statusText, err.response.data)
+        }
+     }
+}
