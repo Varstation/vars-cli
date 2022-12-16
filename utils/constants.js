bf15585a549aa6fc53f3d46610cf3a16bd453297
Luan Vitor Coelho de Araujo <luan_vittor@hotmail.com>
2022-12-16 17:40:28 -0300

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -2,10 +2,10 @@ import { getConstants } from './environment.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const currentFile = fileURLToPath(import.meta.url);
+const currentDir = dirname(currentFile);
 
-export const DATA_FOLDER_PATH = `${dirname(__dirname)}/data`;
+export const DATA_FOLDER_PATH = `${dirname(currentDir)}/data`;
 export const AUTH_PATH = '.auth.json';
 export const AWS_CREDENTIALS_PATH = '.temp_credentials.json';
 export const ENVIRONMENT_SELECTION_PATH = '.environment.json';
