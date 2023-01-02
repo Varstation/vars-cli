77a34f5f4944cefcc01a7caaa0009801be8a6742
Pedro Chazanas <chazanaspln@gmail.com>
2023-01-02 10:49:51 -0300

    error function

--- utils/log.js
+++ utils/log.js
@@ -1,4 +1,4 @@
-export const logFailedRequest = () => {
+export const logFailedRequest = (errorRequest) => {
     console.log('The request failed. Please, try again later. If the error processed to keep happening, please contact our support.');
 };
 
@@ -7,16 +7,21 @@ export const handleDefaultRequestError = (errorRequest) => {
         console.log('You must authenticate to your Varstation account before using this command. Run `vars-cli auth` to authenticate.');
         process.exit();
     } else {
-        logFailedRequest();
+        logFailedRequest(errorRequest);
         process.exit();
     }
 };
 
 export const handleAuthenticationError = (errorRequest) => {
-    if (errorRequest.response?.data?.non_field_errors) {
+    if (errorRequest.response?.data?.nonFieldErrors) {
         console.log('Unable to authenticate. Check your email and password.');
     } else {
-        logFailedRequest();
+        logFailedRequest(errorRequest);
         process.exit();
     }
 };
+
+export const handleGenericError = (error) => {
+    logFailedRequest(error);
+    process.exit();
+}
