ef0c835b77474a58bdfd649da8a48abd9e609d74
Pedro Chazanas <chazanaspln@gmail.com>
2023-09-19 17:51:04 -0300

    send duration on cli

--- utils/aws-credentials.js
+++ utils/aws-credentials.js
@@ -2,14 +2,16 @@ import axios from 'axios';
 import { AWS_CREDENTIALS_PATH, API_URL, AUTH_PATH } from './constants.js';
 import { getJson, saveJson } from './functions.js';
 
-export const getAwsCredentialsRequest = async () => {
+export const getAwsCredentialsRequest = async (duration) => {
     const token = getJson(AUTH_PATH)?.token;
+    let url = `${API_URL}account/user/get_permission_to_upload_cli/`;
+    url = duration ? `${url}?role_duration_seconds=${duration}` : url;
     return await axios.get(
-        `${API_URL}account/user/get_permission_to_upload_cli/`,
+        url,
         {
             headers: {
                 'Authorization': `Token ${token}`
-            }
+            },
         }).then(
         (response) => {
             saveJson(AWS_CREDENTIALS_PATH, response.data);
